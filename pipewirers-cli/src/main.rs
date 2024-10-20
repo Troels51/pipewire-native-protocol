5c87371ca5b0b2064df2a76d42ad26aee5994b3d
Troels Dalsgaard Hoffmeyer <troels.d.hoffmeyer@gmail.com>
2024-10-20 23:35:14 +0200

    Enable more env variables for pipewire socket

--- pipewirers-cli/src/main.rs
+++ pipewirers-cli/src/main.rs
@@ -4,25 +4,24 @@ use pipewire_native_protocol::PipewireClient;
 
 #[tokio::main]
 async fn main() -> tokio::io::Result<()> {
-    // let pipewire_runtime_dir = env::var("PIPEWIRE_RUNTIME_DIR").unwrap();
-    // let pipewire_runtime_dir = "/run/user/1000".to_string();
     let mut address = Option::None;
-    if let Ok(xdg_runtime_dir) = env::var("XDG_RUNTIME_DIR") {
+    if let Ok(pipewire_runtime_dir) = env::var("PIPEWIRE_RUNTIME_DIR") {
+        address = Some(pipewire_runtime_dir + "pipewire-0");
+    } else if let Ok(xdg_runtime_dir) = env::var("XDG_RUNTIME_DIR") {
         address = Some(xdg_runtime_dir + "pipewire-0");
+    } else if let Ok(userprofile) = env::var("USERPROFILE") {
+        address = Some(userprofile + "pipewire-0");
     }
-    let userprofile = env::var("USERPROFILE");
-    dbg!(&address);
     if let Some(address) = address {
         let stream = tokio::net::UnixStream::connect(address).await?;
         let client = PipewireClient::connect(stream).await;
+        // wait for keypress
+        let mut line = String::new();
+        let input = std::io::stdin()
+            .read_line(&mut line)
+            .expect("Failed to read line");
     } else {
         println!("Could not find pipewire socket");
     }
-
-    // wait for keypress
-    let mut line = String::new();
-    let input = std::io::stdin()
-        .read_line(&mut line)
-        .expect("Failed to read line");
     Ok(())
 }
