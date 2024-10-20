909ca7f8df0ddbb32a62c939d6042e1a4720499e
Troels Dalsgaard Hoffmeyer <troels.d.hoffmeyer@gmail.com>
2024-10-20 23:32:54 +0200

    Implement ClientEvent as Event

--- pipewire-native-protocol/src/client.rs
+++ pipewire-native-protocol/src/client.rs
@@ -1,7 +1,12 @@
 use std::collections::HashMap;
 
-use spa::serialize::PodSerializer;
+use spa::{
+    deserialize::{DeserializeError, PodDeserializer},
+    serialize::PodSerializer,
+};
 use spa_derive::{PodDeserialize, PodSerialize};
+
+use crate::opcode;
 
 // === Methods ===
 #[derive(PodSerialize, PodDeserialize, Debug)]
@@ -46,4 +51,34 @@ pub struct Info {
 pub struct Permissions {
     pub index: i32,
     pub permissions: HashMap<i32, i32>,
+}
+
+impl opcode::OpCode for ClientEvent {
+    fn opcode(&self) -> u32 {
+        match self {
+            ClientEvent::Info(_) => 0,
+            ClientEvent::Permissions(_) => 1,
+        }
+    }
+
+    fn deserialize_from_opcode<'de>(
+        opcode: u32,
+        buffer: &'de [u8],
+    ) -> Result<(&'de [u8], Self), spa::deserialize::DeserializeError<&'de [u8]>>
+    where
+        Self: Sized,
+    {
+        match opcode {
+            0 => {
+                let (remain, value): (&[u8], Info) = PodDeserializer::deserialize_from(&buffer)?;
+                Ok((remain, ClientEvent::Info(value)))
+            }
+            1 => {
+                let (remain, value): (&[u8], Permissions) =
+                    PodDeserializer::deserialize_from(&buffer)?;
+                Ok((remain, ClientEvent::Permissions(value)))
+            }
+            _ => Err(DeserializeError::InvalidType),
+        }
+    }
 }
